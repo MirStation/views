d2dc317f0fc4395bf94022cc46d820a038178049
Antonio Martins Miranda <amartmiranda@gmail.com>
2017-02-14 21:44:46 -0200

    Layout adicionado nas views do curso.

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -1,23 +1,19 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>Cursos</title>
-    <link rel="stylesheet" type="text/css" href="/css/app.css" />
-  </head>
-  <body>
-    <div class="container">
-      <div class="row">
-	<div class="col-md-6 col-md-offset-3">
-          <h1>Cursos</h1>
-          <ul class="list-group">
-	    @foreach ($cursos as $curso)
-	    <li class="list-group-item"><a href="/cursos/{{ $loop->index }}">{{ $curso }}</a></li>
-	    @endforeach	    
-	  </ul>  
-        </div>
-      </div>
-    </div>
-    <script type="text/javascript" src="/js/app.js" >
-  </body>
-</html>
+@extends('layout')
+
+@section('title', 'Cursos')
+
+@section('content')
+<div class="row">
+  <div class="col-md-6">
+    
+    <h3>Cursos</h3>
+    
+    <ul class="list-group">
+      @foreach ($cursos as $curso)
+      <li class="list-group-item"><a href="/cursos/{{ $loop->index }}">{{ $curso }}</a></li>
+      @endforeach
+    </ul>
+    
+  </div>
+</div>
+@endsection
